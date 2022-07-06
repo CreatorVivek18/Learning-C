56813da18dd572467a6cae4b5d88eda270a27185
Vivek Kumar <vivekk182btechcse2025@kccitm.edu.in>
2022-07-06 18:42:52 +0530

    to check any string is pallindrome or not

---
+++ stringPallindrome.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+
+    char str[20];
+    int i, len, temp=0;
+    int flag = 0;
+    
+    printf("Enter a word:");
+    scanf("%s", str);
+    
+    len = strlen(str);
+    
+    for(i=0;i < len ;i++){
+        if(str[i] != str[len-i-1]){
+            temp = 1;
+        break;
+   }
+}
+    
+    if (temp==0) {
+        printf("Palindrome");
+    }    
+    else {
+        printf("Not Palindrome");
+    }
+    return 0;
+} 
